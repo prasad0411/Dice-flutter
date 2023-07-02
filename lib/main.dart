97782b3be9ef1c8e470d2e48de03b7ecb72b73aa
Prasad Kanade <65277692+prasad0411@users.noreply.github.com>
2023-07-02 16:20:41 +0530

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ void main() {
   );
 }
 
-
 class DicePage extends StatefulWidget {
   @override
   _DicePageState createState() => _DicePageState();
@@ -27,46 +26,45 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
-  void func()
-  {
-    setState(() 
-    {
-      leftDiceNumber= Random().nextInt(6) +1;
-      rightDiceNumber= Random().nextInt(6) +1;
-    }
-    );
+  // Function to update dice numbers
+  void func() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Center(
       child: Row(
-      children: [ 
-
-
-        Expanded(
-              // ignore: deprecated_member_use
-              child: FlatButton(
-                onPressed:(){
-                  func();
-                } ,
-                child: Image(
-                  image: AssetImage('images/dice$leftDiceNumber.png'),
-            ),
-              ),), 
-
-        Expanded(
+        children: [
+          Expanded(
+            // Button for left dice
             // ignore: deprecated_member_use
             child: FlatButton(
-              onPressed:(){
-                   func();
-              } ,
+              onPressed: () {
+                func();
+              },
+              child: Image(
+                image: AssetImage('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            // Button for right dice
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              onPressed: () {
+                func();
+              },
               child: Image(
                 image: AssetImage('images/dice$rightDiceNumber.png'),
               ),
             ),
-        ),],),
+          ),
+        ],
+      ),
     );
   }
 }
